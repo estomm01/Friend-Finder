b847a4683dd9d40350c9e619dd0fcf67eb3f4446
Markus Maasikas <markus@hotmail.com>
2019-05-13 19:43:38 -0400

    updated routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,16 +1,16 @@
-// The apiRoutes.js file includes two basic routes for app.get function and app.post function which used for displaying a JASON data and incoming survey results of all possible friends:
-// The app.post(in the apiRoutes.js) used to handle the compatible logic.
-
+// The apiRoutes.js file includes two basic routes for app.get function and app.post function which used for JASON data and
+// results of all possible friends:
 
 var friends = require('../data/friends.js');
 
 // Routing the apiRoutes with the app.get and app.post functions
 module.exports = function (app) {
-    // The app.get request handles when user 'visits' a page
+
+    // The app.get request handles when you visit the page
     app.get('/api/friends', function (req, res) {
         res.json(friends);
     });
-    // The app.post request handles when a user submits a form and thus submits data to the surver
+    // The app.post request is for the user when they submit the data to the form
     app.post('/api/friends', function (req, res) {
         // loop through all of the possible options
         var bestMatch = {
